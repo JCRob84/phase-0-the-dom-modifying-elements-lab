52458e90c50043ca3382cf5a3e238936b3d22fb9
Juan Robledo <robledoc@gmail.com>
2023-04-09 23:43:44 -0400

    phase-0-the-dom-modifying-elements-lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+//1) Remove the DOM node 'main#main'
+const main = document.querySelector('#main');
+main.remove();
+
+//2) Create new <h1> element and assign it to a 'newHeader' variable
+const newHeader = document.createElement('h1');
+
+//3) Add an id of 'victory' to the 'newHeader' variable
+newHeader.id = 'victory';
+
+//4) Set the text of 'newHeader' to "YOUR-NAME is the champion"
+newHeader.textContent = "Juan Carlos is the champion";
+
+//Append 'newHeader' to the DOM's <body> element
+document.body.appendChild(newHeader);
